80d316bde11e48d5b27bfc6e4a1402a5154ecbc2
vinay <vinaygundam123@gmail.com>
2025-03-08 23:16:42 +0530

    Initial commit

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 router
   .post('/',userController.createUser)
-  .get('/',userController.getAllUsers)
-  .get('/:id',userController.getUser)
-  .put('/:id',userController.replaceUser)
-  .patch('/:id',userController.updateUser)
-  .delete('/:id',userController.deleteUser);
+  // .get('/',userController.getAllUsers)
+  // .get('/:id',userController.getUser)
+  // .put('/:id',userController.replaceUser)
+  // .patch('/:id',userController.updateUser)
+  // .delete('/:id',userController.deleteUser);
 
 exports.router=router;
